d53622686794dd3b988c36f323c8d8ddbc992ff6
JAYDEN Y H XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEJA3@OOCL.COM>
2020-07-22 17:00:37 +0800

    [ [practice 0] [jayden] feature: finish practice02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,18 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    private int klass;
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return "I am a Student. I am at Class "+ this.klass + ".";
+    }
 }
--- src/test/java/practice02/Practice02Test.java
+++ src/test/java/practice02/Practice02Test.java
@@ -1,35 +1,35 @@
-//package practice02;
-//
-//import org.junit.Test;
-//
-//import static org.fest.assertions.api.Assertions.assertThat;
-//
-//public class Practice02Test {
-//    @Test
-//    public void should_person_have_name_and_age() throws Exception {
-//        Person person = new Person("Tom", 21);
-//        assertThat(person.getName()).isEqualTo("Tom");
-//        assertThat(person.getAge()).isEqualTo(21);
-//    }
-//
-//    @Test
-//    public void should_person_have_an_introduce_method_which_introduce_person_with_name_and_age() throws Exception {
-//        Person tom = new Person("Tom", 21);
-//        String introduce = tom.introduce();
-//        assertThat(introduce).isEqualTo("My name is Tom. I am 21 years old.");
-//    }
-//
-//    @Test
-//    public void should_student_have_name_age_and_class_number() throws Exception {
-//        Student tom = new Student("Tom", 21, 2);
-//        assertThat(tom.getName()).isEqualTo("Tom");
-//        assertThat(tom.getAge()).isEqualTo(21);
-//        assertThat(tom.getKlass()).isEqualTo(2);
-//    }
-//
-//    @Test
-//    public void should_student_introduce_with_class() throws Exception {
-//        Student tom = new Student("Tom", 21, 2);
-//        assertThat(tom.introduce()).isEqualTo("I am a Student. I am at Class 2.");
-//    }
-//}
+package practice02;
+
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class Practice02Test {
+    @Test
+    public void should_person_have_name_and_age() throws Exception {
+        Person person = new Person("Tom", 21);
+        assertThat(person.getName()).isEqualTo("Tom");
+        assertThat(person.getAge()).isEqualTo(21);
+    }
+
+    @Test
+    public void should_person_have_an_introduce_method_which_introduce_person_with_name_and_age() throws Exception {
+        Person tom = new Person("Tom", 21);
+        String introduce = tom.introduce();
+        assertThat(introduce).isEqualTo("My name is Tom. I am 21 years old.");
+    }
+
+    @Test
+    public void should_student_have_name_age_and_class_number() throws Exception {
+        Student tom = new Student("Tom", 21, 2);
+        assertThat(tom.getName()).isEqualTo("Tom");
+        assertThat(tom.getAge()).isEqualTo(21);
+        assertThat(tom.getKlass()).isEqualTo(2);
+    }
+
+    @Test
+    public void should_student_introduce_with_class() throws Exception {
+        Student tom = new Student("Tom", 21, 2);
+        assertThat(tom.introduce()).isEqualTo("I am a Student. I am at Class 2.");
+    }
+}
