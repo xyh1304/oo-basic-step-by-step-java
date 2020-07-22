eef56c4e01d325d94f7025e6d22ebc9913afb4ab
JAYDEN Y H XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEJA3@OOCL.COM>
2020-07-22 18:56:00 +0800

    [practice 08] [jayden] feature: finished practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,28 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+    private String displayName;
+
+    public Klass(int number) {
+        this.number = number;
+        this.displayName = "Class 2";
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student student) {
+        this.leader = student;
+    }
 }
