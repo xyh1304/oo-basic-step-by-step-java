01faf6a5fa968a563c4212e3b93916ad5fa36c57
JAYDEN Y H XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEJA3@OOCL.COM>
2020-07-22 17:13:26 +0800

    [practice 04] [jayden] feature: finish practice 04

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,18 @@
 package practice04;
 
-public class Student {
+public class Student extends Person{
+    private int klass;
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return "My name is "+ this.getName()+". I am "+ this.getAge()+" years old. I am a Student. I am at Class "+this.getKlass()+".";
+    }
 }
--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,12 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person{
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Worker. I have a job.";
+    }
 }
